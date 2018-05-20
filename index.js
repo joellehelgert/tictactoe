cf842118f94f86b10807a360d53b56d06f8b5cf6
xJuyx <joelle.helgert@gmail.com>
2018-05-20 11:24:34 +0200

    initializing project

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-console.log('in index.js')
-ReactDOM.render(
-  '<h1>Hello World with JSX, Babel and Webapck</h1>',
-  document.getElementById("root")
-);
